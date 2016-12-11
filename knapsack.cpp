d3634ba01bf7cfbaed03910697d87055b6b4e89d
KerryHolmes <kjh80@zips.uakron.edu>
2016-12-11 17:40:15 -0500

    Change executable name. fix naming errors

--- knapsack.cpp
+++ knapsack.cpp
@@ -8,7 +8,7 @@ using table = std::vector<std::vector<int>>;
 vector 
 knapsack(vector values, vector weights, int weight_lim)
 {
-    table subproblems = vector(values.size() + 1, vector(weight_lim + 1 , 0));
+    table subproblems = (values.size() + 1, vector(weight_lim + 1 , 0));
     vector solution = vector(values.size(), 0);
 
     for(int i = 1; i < values.size(); ++i)
@@ -22,7 +22,7 @@ knapsack(vector values, vector weights, int weight_lim)
             else
             {
                 subproblems[i][j] = std::max(subproblems[i-1][j], 
-                                    subproblems[i-1][j-w[i]] + values[i]);
+                                    subproblems[i-1][j-weights[i]] + values[i]);
                 solution[i] = 1;
             }
         }
