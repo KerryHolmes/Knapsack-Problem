b046df6977e51b71789977c1fa418c1b3a9a4294
KerryHolmes <kjh80@zips.uakron.edu>
2016-12-11 17:14:23 -0500

    Add base code

--- knapsack.cpp
+++ knapsack.cpp
@@ -8,6 +8,23 @@ using table = std::vector<std::vector<int>>;
 vector 
 knapsack(vector values, vector weights, int weight_lim)
 {
+    table subproblems = vector(values.size() + 1, vector(weight_lim + 1 , 0));
 
+    for(int i = 1; i < values.size(); ++i)
+    {
+        for(int j = 0; j < weight_lim; ++j)
+        {
+            if(weights[i] > j)
+            {
+                subproblems[i][j] = subproblems[i-1][j];
+            }
+            else
+            {
+                subproblems[i][j] = std::max(subproblems[i-1][j], 
+                                    subproblems[i-1][j-w[i]] + values[i]);
+            }
+        }
+    }
+    
     return solution;
 }
