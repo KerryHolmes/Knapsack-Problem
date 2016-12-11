e1d33158cdd476c498527f8f9707251bc05a0639
KerryHolmes <kjh80@zips.uakron.edu>
2016-12-11 17:34:53 -0500

    Add test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include "knapsack.hpp"
 #include <vector>
 #include <iostream>
+#include <random>
 
 int main(int argc, char* argv)
 {
+    std::vector<int> values (10), weights (10);
+    std::srand(0);
+
+    for(int i = 0; i < 10; ++i)
+    {
+        values[i] = rand() % 20;
+        weights[i] = rand() % 10;    
+    }
+
+    for(int i = 0; i < values.size(); ++i)
+        std::cout << i << ": VAL: " << values[i] << " WT: " << weights[i] << std::endl;
+
+    auto result = knapsack(values, weights, 10);
+
+    for(int i = 0; i < result.size(); ++i)
+        std::cout << i << ": " << results[i] << std::endl;
+
     return 0;
 }
