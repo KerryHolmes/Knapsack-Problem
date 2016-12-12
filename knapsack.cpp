6dc35a54d3d7338f644847f757e6858e3c30bd54
KerryHolmes <kjh80@zips.uakron.edu>
2016-12-11 20:12:06 -0500

    correct for loop bounds

--- knapsack.cpp
+++ knapsack.cpp
@@ -11,10 +11,10 @@ knapsack(vector values, vector weights, int weight_lim)
     table subproblems(values.size() + 1, vector(weight_lim + 1 , 0));
     table inclusion(values.size() + 1, vector(weight_lim + 1 , 0));
     vector solution;
-    
-    for(int i = 1; i < values.size(); ++i)
+
+    for(int i = 1; i <= values.size(); ++i)
     {
-        for(int j = 0; j < weight_lim; ++j)
+        for(int j = 0; j <= weight_lim; ++j)
         {
             if(weights[i] > j)
             {
@@ -37,7 +37,7 @@ knapsack(vector values, vector weights, int weight_lim)
 
     for(int i = inclusion.size()-1; i >= 0; --i)
     {
-        if(inclusion[i,W] == 1)
+        if(inclusion[i][W] == 1)
         {
             solution.push_back(i);
             W -= weights[i];
